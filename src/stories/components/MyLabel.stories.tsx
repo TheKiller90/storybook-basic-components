d86803a5b70c2900f010897f8f0e82389160361c
HazelP <antonio.pimentel.hazel@gmail.com>
2022-12-08 00:59:17 -0600

    Chromatic agregado

--- src/stories/components/MyLabel.stories.tsx
+++ src/stories/components/MyLabel.stories.tsx
@@ -7,7 +7,8 @@ export default {
     argTypes: {
         size: { control: 'select' },
         color: { control: 'select' },
-        fontColor: { control: 'color' }
+        fontColor: { control: 'color' },
+        backgroundColor: { control: 'color' }
     }
 } as ComponentMeta<typeof MyLabel>;
 
@@ -41,4 +42,11 @@ export const CustomFontColor = Template.bind({});
 CustomFontColor.args = {
     size: 'h1',
     fontColor: '#5517ac'
+};
+
+export const CustomBackgroundColor = Template.bind({});
+CustomBackgroundColor.args = {
+    size: 'h1',
+    fontColor: '#eeeeee',
+    backgroundColor: '#000000'
 };
